e6b87e1f9c3d76977a0917e6a2ee04662f7f0d39
Mesut Can Gurle <MesutCan.Gurle@n11.com>
2017-01-12 18:16:37 +0300

    closeness centrality is added.

--- centrality.py
+++ centrality.py
@@ -13,9 +13,18 @@ def calculateBetweennessCentrality(session):
 	for degree in degrees:
 		print degree[0],"=", degree[1]
 
+def calculateClosenessCentrality(session):
+	print "closeness centrality:"
+	degrees = session.run("MATCH (a:Deneme), (b:Deneme) WHERE a<>b WITH length(shortestPath((a)-[]-(b))) AS dist, a, b RETURN DISTINCT  a.fullName, sum(1.0/dist) AS close_central ORDER BY close_central DESC ")
+	for degree in degrees:
+		print degree[0],"=", degree[1]
+
+
+
 def main(session):
-	#calculateDegreeCentrality(session)
-	calculateBetweennessCentrality(session)
+	# calculateDegreeCentrality(session)
+	# calculateBetweennessCentrality(session)
+	calculateClosenessCentrality(session)
 	
 
 if __name__ == '__main__':

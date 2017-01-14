c0cab8c6cb2cce1f59df49ab478604feee326407
Mesut Can Gurle <MesutCan.Gurle@n11.com>
2017-01-14 14:52:51 +0300

    eigenvector centrality is added.

--- centrality.py
+++ centrality.py
@@ -1,5 +1,11 @@
 from neo4j.v1 import GraphDatabase, basic_auth
-import csv
+from pyx import *
+import networkx as nx
+import matplotlib.pyplot as plt
+import cypher
+
+
+
 
 def calculateDegreeCentrality(session):
 	print "degree centrality:"
@@ -19,19 +25,28 @@ def calculateClosenessCentrality(session):
 	for degree in degrees:
 		print degree[0],"=", degree[1]
 
+def calculateEigenvectorCentrality(session):
+	print "Eigenvector centrality:"
+	results = cypher.run("MATCH p = ()-[]-() RETURN p", conn="http://neo4j:123456@localhost:7474/db/data")
+	g = results.get_graph()
+	dictionary_centrality = nx.eigenvector_centrality_numpy(g)
+	print sorted(dictionary_centrality.items(), key=lambda x:x[1])
+	nx.draw(g)
+	plt.show()
+	
 def calculatePageRankCentrality(session):
 	print "page rank:"
-	session.run("UNWIND range(1,10) AS round MATCH (n:Deneme) WHERE rand() < 0.1 // 10% probability MATCH (n:Deneme)-[:relation_x*..10]->(m:Deneme) SET m.rank = coalesce(m.rank,0) + 1")
+	session.run("UNWIND range(1,10) AS round MATCH (n:Deneme) WHERE rand() < 0.1  MATCH (n:Deneme)-[:relation_x*..10]->(m:Deneme) SET m.rank = coalesce(m.rank,0) + 1")
 	degrees = session.run("MATCH (n:Deneme) WHERE n.rank is not null return n.fullName, n.rank order by n.rank desc ")
 	for degree in degrees:
 		print degree[0],"=", degree[1]
-
 
 def main(session):
 	# calculateDegreeCentrality(session)
 	# calculateBetweennessCentrality(session)
 	# calculateClosenessCentrality(session)
-	calculatePageRankCentrality(session)
+	calculateEigenvectorCentrality(session)
+	# calculatePageRankCentrality(session)
 	
 
 if __name__ == '__main__':
